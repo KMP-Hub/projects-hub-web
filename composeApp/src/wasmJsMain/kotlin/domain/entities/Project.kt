76d5a16a7f45061d3b423b10bd01f23d81f743d2
Jarvis Lin <admin@jarvislin.com>
2024-07-18 00:18:31 +0800

    Update project model

--- composeApp/src/wasmJsMain/kotlin/domain/entities/Project.kt
+++ composeApp/src/wasmJsMain/kotlin/domain/entities/Project.kt
@@ -1,13 +1,36 @@
 package domain.entities
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 class Project(
+    @SerialName("id")
+    val id: Long,
+    @SerialName("name")
     val name: String,
+    @SerialName("description")
     val description: String,
+    @SerialName("platforms")
     val platforms: List<SupportedPlatform>,
+    @SerialName("types")
+    val types: List<ProjectType>,
+    @SerialName("html_url")
     val url: String,
+    @SerialName("topics")
     val tags: List<String>,
+    @SerialName("stargazers_count")
+    val starCount: Int,
+    @SerialName("updated_at")
+    val updatedAt: String,
+    @SerialName("created_at")
+    val createdAt: String,
 )
 
 enum class SupportedPlatform {
     Android, iOS, Desktop, Web
+}
+
+enum class ProjectType {
+    Library, Showcase
 }
